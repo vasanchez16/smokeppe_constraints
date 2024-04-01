c9dee28fa8c9089b27e3b45e3c9b7c042f9ce755
Victor Sanchez <vsanchez@andrew.cmu.edu>
2024-03-31 20:47:57 -0400

    more code for subregion stuff

--- script/EmulatorEval.py
+++ script/EmulatorEval.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 import matplotlib.patches as patches 
 import os
 from datetime import datetime
@@ -44,12 +45,12 @@ def EmulatorEval(args):
     my_obs_df = pd.read_csv(satellite_file_path,index_col=0)
     my_obs_df.sort_values(['time','latitude','longitude'], inplace=True, ignore_index=True)
 
+    snip_subregion_obs = my_obs_df[my_obs_df['time'] == np.unique(my_obs_df['time'])[0]]
     if toggle_filter:
         plt.figure(figsize=(12,10))
         ax = plt.subplot(111,projection=ccrs.PlateCarree())
-        ax.set_extent([-60,45,-45,15], crs=ccrs.PlateCarree())
         plt.gca().coastlines()
-        gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True)
+        gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, linestyle='--', color='black', alpha=0.5)
 
         rect1 = patches.Rectangle((lon_min, lat_min), 
                                 lon_max - lon_min, 
@@ -57,10 +58,23 @@ def EmulatorEval(args):
                                 linestyle='--', edgecolor='red', facecolor='none', linewidth=4, label='Subregion'
                                 )
         ax.add_patch(rect1)
-        gl.xlabel_style = {'fontsize':20}
-        gl.ylabel_style = {'fontsize':20}
 
-        plt.legend(prop={'size': 18})
+        plt.scatter(
+            snip_subregion_obs['longitude'],
+            snip_subregion_obs['latitude'],
+            color='black'
+        )
+
+        ax.add_feature(cfeature.LAND, facecolor='tan')
+        ax.add_feature(cfeature.LAKES, alpha=0.9, zorder=-1)  
+        ax.add_feature(cfeature.BORDERS, zorder=-1)
+        ax.add_feature(cfeature.COASTLINE, zorder=-1)
+        ax.add_feature(cfeature.OCEAN,alpha=0.5)
+
+        gl.xlabel_style = {'color': 'black', 'weight': 'bold'}
+        gl.ylabel_style = {'color': 'black', 'weight': 'bold'}
+
+        plt.legend(prop={'size': 18}, loc=1)
         plt.title('Subregion Filter', fontsize=30)
         plt.savefig(save_here_dir + 'subregion.png', dpi=300)
 
