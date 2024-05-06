4a2e5353d4822ab51d3052fba89ece81f64b6274
Victor Sanchez <vsanchez@andrew.cmu.edu>
2024-05-06 12:59:36 -0400

    use dists and varis in freq_conf_set #7

--- script/run.py
+++ script/run.py
@@ -77,7 +77,7 @@ def main(args):
     """
     5. Compute confidence sets
     """
-    frequentist_confidence_set(args)
+    frequentist_confidence_set(args, distances, variances)
     print(runtime(time.time() - start_time))
 
     """
