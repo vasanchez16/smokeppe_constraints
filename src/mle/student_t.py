59b14062c96ab64b404c0c39f2a7cb703b4cbb61
Victor Sanchez <vsanchez@andrew.cmu.edu>
2024-09-28 18:27:20 -0400

    small change to the way dists and varis are read in for mle #6

--- src/mle/student_t.py
+++ src/mle/student_t.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import netCDF4 as nc
 import scipy
 from scipy.special import gamma
 import scipy.stats
@@ -23,10 +24,11 @@ def mle_t(args, distances, variances, num_variants):
     bnds = eval_params['MLE_optimization']['bounds']
     init_vals = eval_params['MLE_optimization']['initial_vals']
 
+    run_label = eval_params['run_label']
+    save_here_dir = args.output_dir + run_label + '/'
+
     def minus_log_l(d):
-        # get dists for one param set
-        dists = distances[:,:,:,param_set].flatten() # dims: time, lat, lon, variant
-        varis = variances[:,:,:,param_set].flatten()
+        
         # Log likelihood, to be maximized
         sigma_opt = d[0]
         nu_opt = d[1]
@@ -52,23 +54,22 @@ def mle_t(args, distances, variances, num_variants):
     epsilon_terms = []
     progress_bar = tqdm(total=num_variants, desc="Progress")
     for u in range(num_variants):
-        param_set = u
+        nc_file = nc.Dataset(save_here_dir + 'distances_variances.nc','r')
+        dists = nc_file['distances'][:,:,:,u].flatten()
+        varis = nc_file['variances'][:,:,:,u].flatten()
+
         x_0 = init_vals
         if len(init_vals) > 2:
-            res = minimize(minus_log_l,x_0,bounds=[tuple(bnds[0]),tuple(bnds[1]),tuple(bnds[2])])
+            res = minimize(minus_log_l,x_0,bounds=[tuple(bnds[0]),tuple(bnds[1]),tuple(bnds[2])], method = 'Nelder-Mead')
             epsilon_terms.append(res.x[2])
         else:    
-            res = minimize(minus_log_l,x_0,bounds=[tuple(bnds[0]),tuple(bnds[1])])
+            res = minimize(minus_log_l,x_0,bounds=[tuple(bnds[0]),tuple(bnds[1])], method = 'Nelder-Mead')
         max_l_for_us.append(-res.fun)
         sigma_sqr_terms.append(res.x[0]**2)
         nu_terms.append(res.x[1])
         progress_bar.update(1)
     progress_bar.close()
 
-    with open(args.input_file,'r') as file:
-        eval_params = json.load(file)
-    run_label = eval_params['run_label']
-    save_here_dir = args.output_dir + run_label + '/'
 
     if len(init_vals) > 2:
         all_mle = pd.DataFrame([max_l_for_us,sigma_sqr_terms,nu_terms,epsilon_terms], index = ['log_L', 'sigma_sqr', 'nu', 'epsilon']).transpose()
@@ -79,18 +80,19 @@ def mle_t(args, distances, variances, num_variants):
     # Find parameter set that gives the max likelihood
     u_mle = max_l_for_us.index(max(max_l_for_us)) # param combination number
     # Use this parmeter set to get the model discrep term at that parameter set
-    param_set = u_mle
+    dists = nc_file['distances'][:,:,:,u_mle].flatten()
+    varis = nc_file['variances'][:,:,:,u_mle].flatten()
     x_0 = init_vals
     if len(init_vals) > 2:
-        dec_vars = minimize(minus_log_l,x_0,bounds=[tuple(bnds[0]),tuple(bnds[1]),tuple(bnds[2])]).x #val for model discrep term
+        dec_vars = minimize(minus_log_l,x_0,bounds=[tuple(bnds[0]),tuple(bnds[1]),tuple(bnds[2])], method = 'Nelder-Mead').x #val for model discrep term
     else:
-        dec_vars = minimize(minus_log_l,x_0,bounds=[tuple(bnds[0]),tuple(bnds[1])]).x #val for model discrep term
+        dec_vars = minimize(minus_log_l,x_0,bounds=[tuple(bnds[0]),tuple(bnds[1])], method = 'Nelder-Mead').x #val for model discrep term
     sigma = dec_vars[0]
     sigma_sqr = sigma**2
     nu = dec_vars[1]
     column_names = ['parameter_set_num', 'variance_mle', 'nu']
     optimized_vals = [u_mle, sigma_sqr, nu]
-
+    nc_file.close()
     if len(init_vals) > 2:
         epsilon = dec_vars[2]
         column_names = column_names + ['epsilon']
